50b6994d06a7bc4a53699f2117be08fb5825ba51
Randy Lutcavich <rlutcavich@gmail.com>
2018-06-08 02:50:19 -0700

    - Basic FizzBuzz console program created

--- src/com.rlutcavich/Main.java
+++ src/com.rlutcavich/Main.java
@@ -6,7 +6,40 @@ import com.rlutcavich.utils.ConsoleHelperImpl;
 public class Main {
 
     public static void main(String[] args) {
-        ConsoleHelper consoleHelper = ConsoleHelperImpl.getInstance();
-        consoleHelper.displayMessage("FizzBuzzer Coming Soon");
+        showIntroMessage();
+
+        int count = promptUserForCount();
+
+        runFizzBuzzerFrom1To(count);
     }
+
+    private static void showIntroMessage() {
+        consoleHelper.displayMessage(FIZZBUZZ_DESCRIPTION);
+    }
+
+    private static int promptUserForCount() {
+        String response = consoleHelper.displayMessageAndGetResponse(COUNT_PROMPT_MESSAGE);
+
+        int count = 0;
+        try {
+            count = Integer.valueOf(response);
+        } catch (NumberFormatException e) {
+            consoleHelper.displayMessage(ERROR_MESSAGE_ENTER_NUMBERS_ONLY);
+        }
+        return count;
+    }
+
+    private static void runFizzBuzzerFrom1To(int count) {
+        FizzBuzzer fizzBuzzer = FizzBuzzer.getInstance();
+        for (int i = 1; i <= count; i++) {
+            consoleHelper.displayMessage(fizzBuzzer.getValue(i));
+        }
+    }
+
+    // Constants and Fields
+    private static final String FIZZBUZZ_DESCRIPTION = "A program that prints the numbers from 1 to X. \nBut for multiples of three, \"FIZZ\" is printed instead of the number. \nFor the multiples of five, \"BUZZ\" is printed instead. \nFor numbers which are multiples of both three and five, \"FIZZBUZZ\" is printed.\n";
+    private static final String COUNT_PROMPT_MESSAGE = "How high should the FizzBuzzer go? From 1 to: ";
+    private static final String ERROR_MESSAGE_ENTER_NUMBERS_ONLY = "Enter numbers only.";
+
+    private static ConsoleHelper consoleHelper = ConsoleHelperImpl.getInstance();
 }
